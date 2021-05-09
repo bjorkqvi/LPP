256d327ba000a39c6911c50a62e80694cf78ec6c
Jan-Victor Björkqvist <jvbjorkq@gmail.com>
2021-05-09 15:12:21 +0300

    Added a folder for standardized processing scripts

---
+++ processing_scripts/LPP_acc2disp_30min_Suomenlinna.m
@@ -0,0 +1,72 @@
+%% LPP_acc2disp_30min_Suomenlinna.m
+% -------------------------------------------------------------------------------------------------------------------------------
+% This script converts accelearation data to chopped up 30 mintute displacement files
+% -------------------------------------------------------------------------------------------------------------------------------
+% [Reads]
+%	Netcdf, e.g. netcdf/AccX_Suomenlinna2020_depl_04_01.nc
+% [Writes]
+%	Netcdf, e.g. netcdf/up_30min_Suomenlinna2020_depl_04_01.nc
+% [Pre-processing scripts]
+%	LPP_acc_nr2nc_Suomenlinna.m
+% [Post-processing scripts]
+%	LPP_disp2spec_Suomenlinna.m (doesn't exist yet!)
+% -------------------------------------------------------------------------------------------------------------------------------
+% This script is a part of the LainePoiss Processing package.
+% Jan-Victor Björkqvist & Victor Alari (2021)
+% -------------------------------------------------------------------------------------------------------------------------------
+clear
+close all
+
+addpath('../LPP/functions/')
+run_index='depl_04_01';
+Fn=sprintf('netcdf/AccZ_Suomenlinna2020_%s.nc',run_index); 
+fsIn=50;
+fsOut=5.12;
+desired_length=30; % [min] Final length
+padding=1; % [min] Read this much extra to account for lost data from filtering etc.
+time_shift=60; % Seconds/month
+
+%% Get start time of deployment and read in data
+time0=datetime(ncreadatt(Fn,'/','start_time_posix'),'convertfrom','posixtime');
+az=ncread(Fn,'acc');
+az_time=ncread(Fn,'time');
+
+%% Calculate wanted UTC start times that cover the deployment
+time1=time0+seconds(az_time(end)/1000);
+timevec=[roundto30min(time0)-minutes(desired_length):minutes(desired_length):roundto30min(time1)+minutes(desired_length)]'; % This only works with 30 minute blocks!!!
+
+%% Read blocks 
+%Allow for FIR and transience and shift the desired UTC times to LP-times
+timevec=lpp_shift_time(timevec-minutes(1),time_shift,'UTCtoLP'); 
+block_length=(desired_length+2*padding)*60*fsIn; % Length of block in points
+[block, block_time]=lpp_get_block(az,az_time,time0,timevec,block_length); % Get 32 minute blocks
+
+clear az az_time timevec
+
+%% Go from acceleration to elevation
+%----------------------------------------------------------------------------------------------------
+[block, block_time]=lpp_check_for_gaps(block, block_time,200); % Interpolate gaps shorter than 200 ms
+
+signal_filtered = lpp_fir(block); 
+
+signal_interpolated = lpp_interpolate(signal_filtered,'fsIn',fsIn,'fsOut',fsOut,'method','nearest');
+
+up = lpp_integrate(signal_interpolated,fsOut);
+%----------------------------------------------------------------------------------------------------
+
+%% Cut down to exactly 30 minutes
+fir_cut=round((size(block,1)-size(signal_filtered,1))*fsOut/fsIn); % FIR cut this many points normalized to the final frequency
+int_cut=round(30*fsOut); % Integration cut this many point from each end
+n_start=round(padding*60*fsOut)-fir_cut-int_cut; % Need to cut this much more from start to compensate for that we starten 1 minute becore 00/30.
+n_length=desired_length*60*fsOut; 
+
+up=up(n_start:n_start+n_length-1,:);
+up_time=block_time(n_start:n_start+n_length-1,:);
+
+% Final starting times in UTC to be compared with Waverider
+% These should be very close to 00/30
+timevec_UTC=roundto30min(lpp_shift_time(time0+seconds(up_time(1,:)/1000)',time_shift,'LPtoUTC')+minutes(1)); 
+
+%% Write to netcdf
+Fn=sprintf('netcdf/up_30min_Suomenlinna2020_%s.nc',run_index);
+lpp_write_netcdf_raw(Fn,up,timevec_UTC,'name','Suomenlinna','run_index',run_index);
---
+++ processing_scripts/LPP_acc_nr2nc_Suomenlinna.m
@@ -0,0 +1,64 @@
+%% LPP_acc_nr2nc_Suomenlinna.m
+% -------------------------------------------------------------------------------------------------------------------------------
+% This script reads in NR-files (containing acceleration data) and combines the acceleration to a long single time series
+% -------------------------------------------------------------------------------------------------------------------------------
+% [Reads]
+%	NR-files (original LainePoiss ascii)
+% [Writes]
+%	Netcdf, e.g. netcdf/AccX_Suomenlinna2020_depl_04_01.nc
+% [Pre-processing scripts]
+%	None
+% [Post-processing scripts]
+%	LPP_acc2disp_30min_Suomenlinna.m
+% -------------------------------------------------------------------------------------------------------------------------------
+% This script is a part of the LainePoiss Processing package.
+% Jan-Victor Björkqvist & Victor Alari (2021)
+% -------------------------------------------------------------------------------------------------------------------------------
+
+clear
+close all
+
+addpath('../LPP/functions/');
+
+loc='depl_04_01'; % Choose location (folder name)
+
+system(sprintf('ls %s/*NR*.txt > NR.list',loc));
+filelist.lp=importdata('NR.list');
+
+% Remove boat ride etc.
+n0=14;
+n1=length(filelist.lp)-184;
+
+%% Define vectors
+% Read in first NR just to get the length of the vectors
+fn=filelist.lp{n0};
+nr=lpp_read_nr(fn,'checkgaps',false);
+
+Acc=NaN*ones((n1-n0+1)*length(nr.data.Time),3);
+time=Acc(:,1);
+row=1;
+
+%% Loop through NR-files, read them, and add to a single long block
+for n=n0:n1
+    disp(n)
+    fn=filelist.lp{n};
+    
+    nr=lpp_read_nr(fn,'checkgaps',false);
+    drow=length(nr.data.Time);
+    time(row:row+drow-1)=nr.data.Time;
+    
+    Acc(row:row+drow-1,:)=[nr.data.AccX nr.data.AccY nr.data.AccZ];
+    row=row+drow;
+
+end
+
+% Starting time of deployment
+time0=nr.header.time0;
+
+%% Save data to files
+Fn=sprintf('netcdf/AccX_Suomenlinna2020_%s.nc',loc);
+lpp_write_netcdf_nrdata(Fn,squeeze(Acc(:,1,:)),time,time0,'name','Suomenlinna','run_index',loc);
+Fn=sprintf('netcdf/AccY_Suomenlinna2020_%s.nc',loc);
+lpp_write_netcdf_nrdata(Fn,squeeze(Acc(:,2,:)),time,time0,'name','Suomenlinna','run_index',loc);
+Fn=sprintf('netcdf/AccZ_Suomenlinna2020_%s.nc',loc);
+lpp_write_netcdf_nrdata(Fn,squeeze(Acc(:,3,:)),time,time0,'name','Suomenlinna','run_index',loc);
